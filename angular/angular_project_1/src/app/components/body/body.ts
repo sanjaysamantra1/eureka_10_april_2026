f53a232a452d7ef1b385aef2f9527e6bac9f6091
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-05 20:36:16 +0530

    may 5

--- angular/angular_project_1/src/app/components/body/body.ts
+++ angular/angular_project_1/src/app/components/body/body.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { Databinding } from '../databinding/databinding';
 import { Directives } from '../directives/directives';
+import { DirectiveAssignment } from '../directive-assignment/directive-assignment';
+import { Products } from '../products/products';
 
 @Component({
   selector: 'app-body',
   imports: [
     // Databinding,
-    Directives,
+    // Directives,
+    // DirectiveAssignment
+    Products
   ],
   templateUrl: './body.html',
   styleUrl: './body.css',
