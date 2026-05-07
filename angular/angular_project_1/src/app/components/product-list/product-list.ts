c83c8243196db56d8e4f604c363f3f8877a31181
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-07 20:30:12 +0530

    product list

--- angular/angular_project_1/src/app/components/product-list/product-list.ts
+++ angular/angular_project_1/src/app/components/product-list/product-list.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import productData from './product_data';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { NgxPaginationModule } from 'ngx-pagination';
+import Swal from 'sweetalert2';
+import Snackbar from 'awesome-snackbar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [FontAwesomeModule, NgxPaginationModule, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList {
   productArr = productData;
+  faStar = faStar;
+  
+  performSearch(event: any) {
+    const searchText = event.target.value;
+    this.productArr = productData.filter((product) => {
+      return JSON.stringify(product).includes(searchText);
+    });
+  }
+
+  p: number = 1;
+  sortAsc() {
+    this.productArr.sort((p1, p2) => p1.price - p2.price);
+  }
+  sortDesc() {
+    this.productArr.sort((p1, p2) => p2.price - p1.price);
+  }
+
+  openSweetAlert() {
+    Swal.fire('Good job!', 'You clicked the button!', 'success');
+  }
+  openSnackBar() {
+    new Snackbar('Helloooo, Good Morning', {
+      position: 'top-center',
+      theme: 'light',
+      timeout: 5000,
+      actionText: 'X',
+    });
+  }
 }
