9a970d11e13eaece97efa0e059d2d4723941e695
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-14 19:40:58 +0530

    pipes

--- angular/angular_project_1/src/app/components/pipes-demo/pipes-demo.ts
+++ angular/angular_project_1/src/app/components/pipes-demo/pipes-demo.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RemainingPipe } from '../../custom-pipes/remaining-pipe';
+import { MySortPipe } from '../../custom-pipes/my-sort-pipe';
 
 @Component({
   selector: 'app-pipes-demo',
-  imports: [CommonModule, FormsModule, RemainingPipe],
+  imports: [CommonModule, FormsModule, RemainingPipe,MySortPipe],
   templateUrl: './pipes-demo.html',
   styleUrl: './pipes-demo.css',
 })
@@ -14,10 +15,9 @@ export class PipesDemo {
   mySal = 5000;
   dateObj = new Date();
   user = { name: 'sanjay', role: 'trainer', address: 'Bangalore' };
-
   keepOriginalOrder = () => 0;
-
   cars = ['Tata', 'honda', 'maruti', 'hyundai', 'toyota'];
-
   msg = '';
+
+  numArr = [30, 10, 50, 20, 40];
 }
