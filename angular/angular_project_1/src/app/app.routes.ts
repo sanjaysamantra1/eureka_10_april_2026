c64e91653682e3d1f73e65753341e41344a064b9
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-06-02 20:12:43 +0530

    lazy loading

--- angular/angular_project_1/src/app/app.routes.ts
+++ angular/angular_project_1/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Careers } from './components/careers/careers';
 import { Contactus } from './components/contactus/contactus';
 import { Notfound } from './components/notfound/notfound';
 import { UserList } from './components/user-list/user-list';
-import { ProductList } from './components/product-list/product-list';
+// import { ProductList } from './components/product-list/product-list';
 import { Userdetails } from './components/userdetails/userdetails';
 import { Productdetails } from './components/productdetails/productdetails';
 import { PermanentJobs } from './components/permanent-jobs/permanent-jobs';
@@ -32,7 +32,10 @@ export const routes: Routes = [
   },
   { path: 'users', component: UserList },
   { path: 'userdetails/:id', component: Userdetails },
-  { path: 'products', component: ProductList },
+  {
+    path: 'products',
+    loadComponent: () => import('./components/product-list/product-list').then((x) => x.ProductList)
+  },
   { path: 'productdetails', component: Productdetails },
   {
     path: 'upload',
