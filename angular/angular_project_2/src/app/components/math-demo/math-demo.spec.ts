3a997b21bc3c8ecc8e0fcf411179d9386437e3f5
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-06-04 20:35:04 +0530

    june 4

--- angular/angular_project_2/src/app/components/math-demo/math-demo.spec.ts
+++ angular/angular_project_2/src/app/components/math-demo/math-demo.spec.ts
@@ -5,8 +5,15 @@ import { MathDemo } from './math-demo';
 describe('MathDemo', () => {
   let component: MathDemo;
   let fixture: ComponentFixture<MathDemo>;
+  let time;
+
+  beforeAll(() => {
+    time = new Date().toLocaleTimeString();
+    console.log('Before All...'); // called only once, initialize variable once
+  });
 
   beforeEach(async () => {
+    console.log('Before Each...'); // before Every it()
     await TestBed.configureTestingModule({
       imports: [MathDemo],
     }).compileComponents();
@@ -16,11 +23,21 @@ describe('MathDemo', () => {
     await fixture.whenStable();
   });
 
+  afterEach(() => {
+    console.log('After Each...'); // after every it()
+  });
+
+  afterAll(() => {
+    console.log('After All'); // once, after all the it() are executed
+  });
+
   it('should create', () => {
+    console.log('It-1');
     expect(component).toBeTruthy();
   });
 
   it('should verify add function', () => {
+    console.log('It-2');
     expect(component.add(10, 20)).toBe(30);
     expect(component.add(-10, 20)).toBe(10);
     expect(component.add(-10, -20)).toBe(-30);
@@ -28,6 +45,7 @@ describe('MathDemo', () => {
   });
 
   it('should verify isEven function', () => {
+    console.log('It-3');
     expect(component.isEVen(4)).toBe(true);
     expect(component.isEVen(4)).not.toBe(false);
 
@@ -36,6 +54,7 @@ describe('MathDemo', () => {
   });
 
   it('should verify sumOfDigits function', () => {
+    console.log('It-4');
     expect(component.sumOfDigits(125)).toBe(8);
   });
 });
