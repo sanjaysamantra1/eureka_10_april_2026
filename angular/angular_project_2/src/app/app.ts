9c2eab3bf981729f7510748e7832c75c0e28d106
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-06-08 20:35:54 +0530

    june 8

--- angular/angular_project_2/src/app/app.ts
+++ angular/angular_project_2/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 // import { MathDemo } from "./components/math-demo/math-demo";
 import { Counter } from './components/counter/counter';
+import { TodoCrud } from './components/todo-crud/todo-crud';
 
 @Component({
   selector: 'app-root',
   imports: [
     // MathDemo,
-    Counter
+    // Counter
+    TodoCrud
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
